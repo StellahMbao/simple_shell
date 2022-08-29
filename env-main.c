a807d6a30b40e83785dcd0f7f460e29f5e419288
StellahMbao <woodzdj@gmail.com>
2022-08-29 00:27:45 -0700

    Project files

---
+++ env-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the environment
+ *
+ * Return: Always 0
+ */
+int main(int ac, char **av, char **env)
+{
+	unsigned int i;
+
+	i = 0;
+	while (env[i] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
+	return (0);
+}
+
