b5b4b7fbcf44970c33be7201be72c661b0d9e6fd
StellahMbao <woodzdj@gmail.com>
2022-09-07 06:46:12 -0700

    main.h updates

--- main.h
+++ main.h
@@ -1,6 +1,17 @@
 #ifndef MAIN_h
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
 
 #endif
